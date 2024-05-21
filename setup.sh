87de9006e58e82c0d405eb78f536ea395e56bcdc
Edbert-Suwandy <suwandy.edbert@gmail.com>
2024-05-21 21:01:45 +0100

    yes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,22 @@
-#!/bin/sh
+echo "installing packages with homebrew ==>"
 
-brew list zoxide || install zoxide
+brew list zsh || brew install zsh
+brew list zoxide || brew install zoxide
+brew list fzf || brew install fzf 
+brew list neovim || brew install neovim
+brew list stow || brew install stow
 brew list powerlevel10k || brew install powerlevel10k
 brew list ripgrep || brew install ripgrep
-brew list fzf || brew install fzf
+
+echo "setting up zsh as main shell"
+sudo chsh -s $(which zsh)
+
+echo "setting up stow config"
+cdir="$(pwd)"
+
+cd ~/.stow-config
+stow .
+cd $CDIR
+
+echo "Setup Finished"
+
