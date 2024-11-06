3a4aabf764cf4f20dcafe4517c7a66be1ae3a118
Edbert-Suwandy <suwandy.edbert@gmail.com>
2024-11-06 09:26:35 +0000

    .zshrc update completions for most, kube tools

--- .zshrc
+++ .zshrc
@@ -4,12 +4,13 @@ eval "$(starship init zsh)"
 [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
 plug "zsh-users/zsh-autosuggestions"
 plug "zsh-users/zsh-syntax-highlighting"
-plug "ajeetdsouza/zoxide"
 
 autoload -Uz compinit
 compinit
 
 # Zioxide =============================================================================================================
+eval "$(zoxide init zsh)"
+
 # Themes and fonts ====================================================================================================
 
 ZSH_THEME="robbyrussell"
@@ -33,8 +34,18 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu no
 
+#kubeneretes completions
+source <(helm completion zsh)
+source <(kubectl completion zsh)
+source <(kind completion zsh)
 # Aliases
 alias ls="ls --color"
 alias k="kubectl"
 alias vim="nvim"
 alias tf="terraform"
+alias cd="z"
+
+# Kubernetes stuff
+alias kns="kubens"
+alias kctx="kubectx"
+
--- script/setup.sh
+++ script/setup.sh
@@ -8,22 +8,26 @@ if [ $SHELL != "/bin/zsh" && $SHELL != "/usr/bin/zsh"  ]; then
 fi
 echo "zsh shell is in used"
 
-echo "installing homebrew ==>"
-# install gcc if on lunix-gnu
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/edbert/.zshrc
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if [[ "$OSTYPE" == "Darwin"* ]]; then
+		echo "installing homebrew ==>"
+		# install gcc if on lunix-gnu
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/edbert/.zshrc
+		eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
+
 mv ~/.zshrc ~/.zshrc.bak
 source ~/.stow-config/.zshrc
 
+
+if [[ "$OSTYPE" == "Darwin"* ]]; then
 echo "installing homebrew packages ==>"
-brew list stow || brew install stow
-brew list zoxide || brew install zoxide
-brew list fzf || brew install fzf 
-brew list neovim || brew install neovim
+		brew list stow || brew install stow
+		brew list zoxide || brew install zoxide
+		brew list fzf || brew install fzf 
+		brew list neovim || brew install neovim
+fi
 
 echo "setting up stow"
 CDIR= $(PWD)
