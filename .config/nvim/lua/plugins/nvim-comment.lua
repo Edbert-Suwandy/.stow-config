ea4c0de66a8d61dd9a847c66151eb0822f1125a3
Edbert-Suwandy <suwandy.edbert@gmail.com>
2024-11-11 20:21:19 +0000

    pain?

--- .config/nvim/lua/plugins/nvim-comment.lua
+++ .config/nvim/lua/plugins/nvim-comment.lua
@@ -1,13 +1,6 @@
 return {
-	"terrortylor/nvim-comment",
-	config = function ()
-		  marker_padding = true
-		  comment_empty = true
-		  comment_empty_trim_whitespace = true
-		  create_mappings = true
-		  line_mapping = "gcc"
-		  operator_mapping = "gc"
-		  comment_chunk_text_object = "ic"
-		  hook = nil
-       end
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    }
 }
